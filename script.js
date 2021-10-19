281ba4e64e44c0573a0f0bafe53127f401d72240
Noa Udler <udlernoa4@gmail.com>
2021-10-18 21:23:33 -0700

    Adding to basic outline

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").append(h2);
+const clickherefirst = document.getElementById('clickherepicfirstquestion');
+
+function clickAlert() {
+    alert('I am a 24 year-old college graduate from UCLA amidst a career change.')
+}
+
+clickherefirst.addEventListener('click', clickAlert);
+
+const clickheresecond = document.getElementById('clickherepicsecondquestion');
+
+function clickAlert() {
+    alert('I love to cook, sing, study psychology, and work on my coding skills!')
+}
+
+clickheresecond.addEventListener('click', clickAlert);
+
+
+
