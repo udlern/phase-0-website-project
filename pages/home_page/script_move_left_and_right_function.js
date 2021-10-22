c8dab4c53bba56df6c938329e1081db4095f0c87
Noa Udler <udlernoa4@gmail.com>
2021-10-22 10:09:55 -0700

    Re-sizing entire website project

--- pages/home_page/script_move_left_and_right_function.js
+++ pages/home_page/script_move_left_and_right_function.js
@@ -2,16 +2,16 @@
 
 const personalImage = document.getElementById("personal-image");
 
-personalImage.style.height = "250px";
+personalImage.style.height = "150px";
 personalImage.style.left = "1200px";
-personalImage.style.paddingTop = "100px";
+personalImage.style.paddingTop = "130px";
 
 function movePersonalImageLeft() {
   console.log("hello");
   const leftNumbers = personalImage.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
 
-  if (left > 570) {
+  if (left > 575) {
     personalImage.style.left = `${left - 30}px`;
   }
 }
